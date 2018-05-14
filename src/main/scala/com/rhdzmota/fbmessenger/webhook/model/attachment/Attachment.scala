25d8fb6c39e4296bdd75910437dec00914c8a965
rhdzmota <rohdzmota@gmail.com>
2018-05-14 13:55:55 -0500

    delete imports for implicit json conversion

--- src/main/scala/com/rhdzmota/fbmessenger/webhook/model/attachment/Attachment.scala
+++ src/main/scala/com/rhdzmota/fbmessenger/webhook/model/attachment/Attachment.scala
@@ -1,18 +1,11 @@
 package com.rhdzmota.fbmessenger.webhook.model.attachment
 
-import io.circe.Json
-import io.circe.generic.auto._
-import io.circe.syntax._
 
 sealed trait Attachment {
   def `type`: String
   def payload: Option[Payload]
-  def toJson: Json = this.asJson
-  override def toString: String = this.asJson.toString()
 }
 
 case class SimpleAttachment(`type`: String, payload: Option[Url]) extends Attachment
-
 case class Fallback(`type`: String, payload: Option[Payload], title: String, URL: String) extends Attachment
-
 case class Location(title: String, url: String, `type`: String, payload: Option[LocationPayload]) extends Attachment
