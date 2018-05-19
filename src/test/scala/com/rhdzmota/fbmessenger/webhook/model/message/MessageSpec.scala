c44d91702314028c344f1d8021b02f3fdbf308b0
rhdzmota <rohdzmota@gmail.com>
2018-05-18 20:24:56 -0500

    noted problem with sealed traits and decoders

--- src/test/scala/com/rhdzmota/fbmessenger/webhook/model/message/MessageSpec.scala
+++ src/test/scala/com/rhdzmota/fbmessenger/webhook/model/message/MessageSpec.scala
@@ -12,12 +12,20 @@ import com.rhdzmota.fbmessenger.webhook.model.implicits.Decoders._
 class MessageSpec extends FlatSpec with Matchers {
 
       "A Message with Text attachment" should "jsonify correctly" in {
-        val quickReply = QuickReply("DEVELOPER_DEFINED_PAYLOAD")
+        val quickReply = QuickReply("<DEVELOPER_DEFINED_PAYLOAD>")
         val text = Text("<MID>", "<TEXT>", quickReply)
         val message = Message(Participant("<PSID>"), Participant("<PAGE_ID>"), 0, text)
-        val jsonString = "{\"sender\": {\"id\": \"<PSID>\"}, \"recipient\": {\"id\": \"<PAGE_ID>\"}, \"timestamp\": 0, " +
-          "\"message\": {\"mid\": \"<MID>\", \"text\": \"<TEXT>\", \"quick_reply\": " +
-          "{\"payload\": \"DEVELOPER_DEFINED_PAYLOAD\"}}}"
+        val textMessageJson = "{\"mid\": \"<MID>\", \"text\": \"<TEXT>\", \"quick_reply\": {\"payload\": \"<DEVELOPER_DEFINED_PAYLOAD>\"}}"
+        val jsonString = "{\"sender\": {\"id\": \"<PSID>\"}, \"recipient\": {\"id\": \"<PAGE_ID>\"}, \"timestamp\": 0, \"message\": " + textMessageJson + "}"
+        decode[Text](textMessageJson) shouldBe 'right
+        val result = decode[Message](jsonString)
+        result shouldBe 'right
+        result match {
+          case Left(error) => fail(s"failure: ${error.toString}")
+          case Right(x)    =>
+            x shouldBe message
+            x.asJson shouldBe message
+        }
         decode[Message](jsonString).foreach(x => {
       x shouldBe message
       x.asJson shouldBe message.asJson
