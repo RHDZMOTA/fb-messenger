c8032280ba286354240dc0357285d53bcff93239
rhdzmota <rohdzmota@gmail.com>
2018-05-18 20:23:37 -0500

    fix tests

--- src/test/scala/com/rhdzmota/fbmessenger/webhook/model/attachment/AttachmentSpec.scala
+++ src/test/scala/com/rhdzmota/fbmessenger/webhook/model/attachment/AttachmentSpec.scala
@@ -13,19 +13,27 @@ class AttachmentSpec extends FlatSpec with Matchers {
     val url = Url("<ATTACHMENT_URL>")
     val simpleAttachment = SimpleAttachment("image", Some(url))
     val jsonString = "{\"type\": \"image\", \"payload\": {\"url\": \"<ATTACHMENT_URL>\"}}"
-    decode[SimpleAttachment](jsonString).foreach(x => {
-      x shouldBe simpleAttachment
-      x.asJson shouldBe simpleAttachment.asJson
-    })
+    val result = decode[SimpleAttachment](jsonString)
+    result shouldBe ('right)
+    result match {
+      case Left(error) => error
+      case Right(x)    =>
+        x shouldBe simpleAttachment
+        x.asJson shouldBe simpleAttachment.asJson
+    }
   }
 
   "A Fallback(...)" should "jsonify correctly" in {
     val fallback = Fallback("fallback", None, "<TITLE>", "<URL>")
-    val jsonString = "{\"type\": \"fallback\", \"payload\": null, \"title\": \"<TITLE>, \"URL\": \"<URL>\"}"
-    decode[Fallback](jsonString).foreach(x => {
-      x shouldBe fallback
-      x.asJson shouldBe fallback.asJson
-    })
+    val jsonString = "{\"type\": \"fallback\", \"payload\": null, \"title\": \"<TITLE>\", \"URL\": \"<URL>\"}"
+    val result = decode[Fallback](jsonString)
+    result shouldBe 'right
+    result match {
+      case Left(error) => error
+      case Right(x)    =>
+        x shouldBe fallback
+        x.asJson shouldBe fallback.asJson
+    }
   }
 
   "A Location(...)" should "jsonify correctly" in {
@@ -33,10 +41,14 @@ class AttachmentSpec extends FlatSpec with Matchers {
     val locationPayload = LocationPayload(coordinates)
     val location = Location("<TITLE>", "<URL>", "location", Some(locationPayload))
     val jsonString = "{\"title\": \"<TITLE>\", \"url\": \"<URL>\", \"type\": \"location\", \"payload\": {\"coordinates\": {\"lat\": 0.0, \"long\": 0.0}}}"
-    decode[Location](jsonString).foreach(x => {
-      x shouldBe location
-      x.asJson shouldBe location.asJson
-    })
+    val result = decode[Location](jsonString)
+    result shouldBe 'right
+    result match {
+      case Left(error) => error
+      case Right(x)    =>
+        x shouldBe location
+        x.asJson shouldBe location.asJson
+    }
   }
 
 }
--- src/test/scala/com/rhdzmota/fbmessenger/webhook/model/attachment/PayloadSpec.scala
+++ src/test/scala/com/rhdzmota/fbmessenger/webhook/model/attachment/PayloadSpec.scala
@@ -12,20 +12,28 @@ class PayloadSpec extends FlatSpec with Matchers {
   "An Url payload " should "jsonify correctly" in {
     val url = Url("<ATTACHMENT_URL>")
     val jsonString = "{\"url\": \"<ATTACHMENT_URL>\"}"
-    decode[Url](jsonString).foreach(x => {
-      x shouldBe url
-      x.asJson shouldBe url.asJson
-    })
+    val result = decode[Url](jsonString)
+    result shouldBe 'right
+    result match {
+      case Left(error) => error
+      case Right(x)    =>
+        x shouldBe url
+        x.asJson shouldBe url.asJson
+    }
   }
 
   "A Location payload" should "jsonify corretly" in {
     val coordinates = Coordinates(0.0, 0.0)
     val locationPayload = LocationPayload(coordinates)
     val jsonString = "{\"coordinates\": {\"lat\": 0.0, \"long\": 0.0}}"
-    decode[LocationPayload](jsonString).foreach(x => {
-      x shouldBe locationPayload
-      x.asJson shouldBe locationPayload.asJson
-    })
+    val result = decode[LocationPayload](jsonString)
+    result shouldBe 'right
+    result match {
+      case Left(error) => error
+      case Right(x)    =>
+        x shouldBe locationPayload
+        x.asJson shouldBe locationPayload.asJson
+    }
   }
 
 }
