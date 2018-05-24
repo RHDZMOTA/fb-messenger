0b40193c117348aea9e5374b92b9ba62359d7c59
rhdzmota <rohdzmota@gmail.com>
2018-05-24 15:25:42 -0500

    add implicit encoders/decoders for attachments

--- src/main/scala/com/rhdzmota/fbmessenger/send/model/implicits/Decoders.scala
+++ src/main/scala/com/rhdzmota/fbmessenger/send/model/implicits/Decoders.scala
@@ -1,9 +1,11 @@
 package com.rhdzmota.fbmessenger.send.model.implicits
 
+import com.rhdzmota.fbmessenger.send.model.attachment.{Attachment, Element}
+import com.rhdzmota.fbmessenger.send.model.attachment.button._
+import com.rhdzmota.fbmessenger.send.model.attachment.payload._
 import com.rhdzmota.fbmessenger.send.model.reply._
 import com.rhdzmota.fbmessenger.send.model.message._
 import com.rhdzmota.fbmessenger.send.model.message.quickreply._
-
 import io.circe.Decoder
 import io.circe.generic.extras.Configuration
 import io.circe.generic.extras.semiauto.deriveDecoder
@@ -18,12 +20,32 @@ object Decoders {
     Decoder[Text].map[QuickReply](identity).or(Decoder[Other].map[QuickReply](identity))
   // MESSAGE
   implicit val decodeWithText: Decoder[WithText] = deriveDecoder[WithText]
-  //implicit val decodeWithAttachment: Decoder[WithAttachment] = deriveDecoder[WithAttachment]
+  implicit val decodeWithAttachment: Decoder[WithAttachment] = deriveDecoder[WithAttachment]
   implicit val decodeMessage: Decoder[Message] =
-    Decoder[WithText].map[Message](identity)//.or(Decoder[WithAttachment].map[Message](identity))
+    Decoder[WithText].map[Message](identity).or(Decoder[WithAttachment].map[Message](identity))
   // REPLY
   implicit val decodeWithMessage: Decoder[WithMessage] = deriveDecoder[WithMessage]
   implicit val decodeWithAction: Decoder[WithAction] = deriveDecoder[WithAction]
   implicit val decodeReply: Decoder[Reply] =
     Decoder[WithMessage].map[Reply](identity).or(Decoder[WithAction].map[Reply](identity))
+  // BUTTONS
+  implicit val decodeUrl: Decoder[Url] = deriveDecoder[Url]
+  implicit val decodePostback: Decoder[PostbackCall] = deriveDecoder[PostbackCall]
+  implicit val decodeShare: Decoder[Share] = deriveDecoder[Share]
+  implicit val decodeButton: Decoder[Button] =
+    Decoder[Url].map[Button](identity).or(Decoder[PostbackCall].map[Button](identity)).or(Decoder[Share].map[Button](identity))
+  // ELEMENT
+  implicit val decodeElement: Decoder[Element] = deriveDecoder[Element]
+  // PAYLOAD
+  implicit val decodeRichMedia: Decoder[RichMedia] = deriveDecoder[RichMedia]
+  implicit val decodeGenericTemplate: Decoder[GenericTemplate] = deriveDecoder[GenericTemplate]
+  implicit val decodeButtomTemplate: Decoder[ButtonTemplate] = deriveDecoder[ButtonTemplate]
+  implicit val decodeListTemplate: Decoder[ListTemplate] = deriveDecoder[ListTemplate]
+  implicit val decodePayload: Decoder[Payload] =
+    Decoder[ListTemplate].map[Payload](identity)
+      .or(Decoder[GenericTemplate].map[Payload](identity))
+      .or(Decoder[ButtonTemplate].map[Payload](identity))
+      .or(Decoder[RichMedia].map[Payload](identity))
+  // ATTACHMENT
+  implicit val decodeAttachment: Decoder[Attachment] = deriveDecoder[Attachment]
 }
--- src/main/scala/com/rhdzmota/fbmessenger/send/model/implicits/Encoders.scala
+++ src/main/scala/com/rhdzmota/fbmessenger/send/model/implicits/Encoders.scala
@@ -1,9 +1,11 @@
 package com.rhdzmota.fbmessenger.send.model.implicits
 
+import com.rhdzmota.fbmessenger.send.model.attachment.{Attachment, Element}
+import com.rhdzmota.fbmessenger.send.model.attachment.button.{Button, PostbackCall, Share, Url}
+import com.rhdzmota.fbmessenger.send.model.attachment.payload._
 import com.rhdzmota.fbmessenger.send.model.reply._
 import com.rhdzmota.fbmessenger.send.model.message._
 import com.rhdzmota.fbmessenger.send.model.message.quickreply._
-
 import io.circe.Encoder
 import io.circe.syntax._
 import io.circe.generic.extras.Configuration
@@ -21,10 +23,10 @@ object Encoders {
   }
   // MESSAGE
   implicit val encodeWithText: Encoder[WithText] = deriveEncoder[WithText]
-  //implicit val decodeWithAttachment: Encoder[WithAttachment] = deriveEncoder[WithAttachment]
+  implicit val decodeWithAttachment: Encoder[WithAttachment] = deriveEncoder[WithAttachment]
   implicit val encodeMessage: Encoder[Message] = Encoder.instance {
     case withText @ WithText(_, _)             => withText.asJson
-    case _ => "{}".asJson //withAttachment @ WithAttachment(_, _) => "{}".asJson
+    case withAttachment @ WithAttachment(_, _) => withAttachment.asJson
   }
   // REPLY
   implicit val encodeWithMessage: Encoder[WithMessage] = deriveEncoder[WithMessage]
@@ -33,4 +35,28 @@ object Encoders {
     case withMessage @ WithMessage(_, _, _, _) => withMessage.asJson
     case withAction @ WithAction(_, _)         => withAction.asJson
   }
+  // BUTTONS
+  implicit val encodeUrl: Encoder[Url] = deriveEncoder[Url]
+  implicit val encodeShare: Encoder[Share] = deriveEncoder[Share]
+  implicit val encodePostbackCall: Encoder[PostbackCall]= deriveEncoder[PostbackCall]
+  implicit val encodeButton: Encoder[Button] = Encoder.instance {
+    case url @ Url(_, _, _, _, _) => url.asJson
+    case share @ Share(_) => share.asJson
+    case postbackCall @ PostbackCall(_, _, _) => postbackCall.asJson
+  }
+  // ELEMENT
+  implicit val encodeElement: Encoder[Element] = deriveEncoder[Element]
+  // PAYLOAD
+  implicit val encodeRichMedia: Encoder[RichMedia] = deriveEncoder[RichMedia]
+  implicit val encodeGenericTemplate: Encoder[GenericTemplate] = deriveEncoder[GenericTemplate]
+  implicit val encodeButtonTemplate: Encoder[ButtonTemplate] = deriveEncoder[ButtonTemplate]
+  implicit val encodeListTemplate: Encoder[ListTemplate] = deriveEncoder[ListTemplate]
+  implicit val encodePayload: Encoder[Payload] = Encoder.instance {
+    case richMedia @ RichMedia(_) => richMedia.asJson
+    case genericTemplate @ GenericTemplate(_, _, _, _) => genericTemplate.asJson
+    case buttonTemplate @ ButtonTemplate(_, _, _, _) => buttonTemplate.asJson
+    case listTemplate @ ListTemplate(_, _, _, _, _) => listTemplate.asJson
+  }
+  // ATTACHMENT
+  implicit val encodeAttachment: Encoder[Attachment] = deriveEncoder[Attachment]
 }
